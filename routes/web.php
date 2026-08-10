6fa5a80a3f365fa78f410fca98da9ca5275aafdd
Ain Cortés Catoni <aincatoni@uc.cl>
2026-08-09 21:35:10 -0400

    Add login and registration views with form handling

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-    Route::post('/register', [AuthController::class, 'register'])->name('register.store');revi
+    Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 });
 
 Route::middleware('auth')->group(function () {
